fd2f75a9289bc8405c33eb0b2e180ec8115e2997
Olezham <64526832+Olezham@users.noreply.github.com>
2020-07-28 11:36:15 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,7 +22,7 @@ client.remove_command('help')
 
 async def on_ready():
     print( 'Здорова суки!Батя в здании' )
-    await client.change_precence( status = discord.Status.online, activity = discord.Game('Трясёт сиськами'))
+    await client.change_presence( status = discord.Status.online, activity = discord.Game('Трясёт сиськами'))
 #@client.command(pass_contex = True)
 #async def test ( ctx ):
    # await ctx.send('Write message')
