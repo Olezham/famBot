99626d2cf605d7be96eacb3f23d29b8a58cc3f52
Olezham <64526832+Olezham@users.noreply.github.com>
2022-01-02 23:32:11 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -60,7 +60,7 @@ async def call(ctx, member: discord.Member):
 async def help(ctx):
     emb = discord.Embed( title = 'Комманды сервера', colour = discord.Color.green() )
     await ctx.channel.purge (limit = 1)
-    emb.add_field(name = f'call', value  'Позвать пользователя в дискорд')
+    emb.add_field(name = f'call', value = 'Позвать пользователя в дискорд')
     emb.add_field(name = f'kick', value = 'Кикнуть пользователя с сервера')
     emb.add_field(name = f'roll', value = 'Кинуть кубик')
     emb.add_field(name = f'mute', value = 'Замутить пользователя')
